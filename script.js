afe3905b41b31d680e76455d839d0dca1ea29241
Xuyang Zhang <zhangxuyang.chn@gmail.com>
2022-01-27 22:44:03 -0500

    worked on desired layout with static HTML.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Get current day, output to Jumbotron
+$("#currentDay").text(moment().format('dddd [, ] MMM Do YYYY'));
+
+//Show Today Planner
+
+
+
+//Read from localStorage
+
+
+
+//Save to local storage on button click
+
+
+
