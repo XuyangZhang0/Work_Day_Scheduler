ad0cc22ce520768d961061d49f925f7906b964e8
Xuyang Zhang <zhangxuyang.chn@gmail.com>
2022-01-30 15:52:48 -0500

    completed datepicker function to allow going to past dates and future dates to view and set event descriptions. Also added logic to tell future, past, current.

--- script.js
+++ script.js
@@ -5,19 +5,43 @@ let currentHour = getCurrentHour();
 let selectedDay = moment().format('MMDDYYYY');
 let calendarEvents = [];
 let todayEvents = [];
-let calendarEvent = {
-    day: "",
-    hour: 0,
-    description: ""
-}
 
+/*
+Utility or helper functions
+*/
+//Convert 24 hr time to 12 hr
+function convertTime(hour) {
+    return moment(hour, "HH").format("h a");
+}
+// Get current hour number
+function getCurrentHour() {
+    return parseInt(moment().format("H"));
+}
+//Read from localstorage to find current day's events array
+function getTodayEvents() {
+    if (calendarEvents !== null) {
+        todayEvents = calendarEvents.filter(obj => {
+            return obj.day === selectedDay;
+        })
+    }
+}
+// convert date to standard date
+function convertDate(dateString) {
+    if (dateString.length > 1) {
+        return dateString.slice(-4) + dateString.slice(0, -4);
+    }
+    return dateString;
+}
+/*
+Page rendering and event listeners
+*/
 //Show Today Planner
 function showTodayPlanner() {
     for (let i = businessHourStart; i < businessHourEnd; i++) {
         let timeBlockStatus = "";
-        if (currentHour > i) {
+        if (moment().isAfter(convertDate(selectedDay), "day") || (currentHour > i && selectedDay === moment().format('MMDDYYYY'))) {
             timeBlockStatus = "past";
-        } else if (currentHour === i) {
+        } else if (currentHour === i && selectedDay === moment().format('MMDDYYYY')) {
             timeBlockStatus = "present";
         } else {
             timeBlockStatus = "future";
@@ -39,31 +63,26 @@ function showTodayPlanner() {
     }
 }
 
-//Convert 24 hr time to 12 hr
-
-function convertTime(hour) {
-    return moment(hour, "HH").format("h a");
-}
-// Get current hour number
-function getCurrentHour() {
-    return parseInt(moment().format("H"));
-}
-
 //Save to local storage on button click
 $("#planner-view").click(function (event) {
-
+    let calendarEvent = {
+        day: "",
+        hour: 0,
+        description: ""
+    }
     var element = event.target;
     if (element.matches(".saveBtn") || element.matches(".fa-save")) {
         calendarEvent.day = selectedDay;
         calendarEvent.hour = element.dataset.buttonid;
         calendarEvent.description = $(".row").find(`[data-descriptionid="${calendarEvent.hour}"]`).val();
-        if (todayEvents.filter(obj => { return obj.hour == calendarEvent.hour }).length === 0) {
+        if (todayEvents.filter(obj => { return obj.hour == element.dataset.buttonid }).length === 0) {
             calendarEvents.push(calendarEvent);
-            console.log("0 event, so pushed the update");
-        } else {
-            todayEvents.filter(obj => { return obj.hour == calendarEvent.hour })[0].description = $(".row").find(`[data-descriptionid="${calendarEvent.hour}"]`).val();
+            getTodayEvents();
+      } else {
+            todayEvents.filter(obj => { return obj.hour == element.dataset.buttonid })[0].description = $(".row").find(`[data-descriptionid="${element.dataset.buttonid}"]`).val();
         }
         localStorage.setItem("calendarEvents", JSON.stringify(calendarEvents));
+
     }
 });
 
@@ -78,23 +97,27 @@ $("#save-customization").click(function () {
     showTodayPlanner();
 });
 
-//Read from localstorage to find current day's events array
-function getTodayEvents() {
-    if (calendarEvents !== null) {
-        todayEvents = calendarEvents.filter(obj => {
-            return obj.day === selectedDay;
-        })
-    }
+// jQueryUI datepicker 
+$(function () {
+    $("#datepicker").datepicker({
+        onSelect: function (date, datepicker) {
+            if (date != "") {
+                selectedDay = date.replace(/\//g, '');
+                $(".row").remove();
+                getTodayEvents();
+                $("#currentDay").text(moment(selectedDay, "MMDDYYYY").format('dddd [, ] MMM Do YYYY'));
+                showTodayPlanner();
+            }
+        }
+    });
+});
 
-}
-
-//Initialization
+/*
+Initialization (first load or refresh)
+*/
 $("#currentDay").text(moment().format('dddd [, ] MMM Do YYYY'));
-businessHourStart = parseInt(localStorage.getItem("business-hour-start"));
-businessHourEnd = parseInt(localStorage.getItem("business-hour-end"));
-calendarEvents = JSON.parse(localStorage.getItem("calendarEvents"));
-if (calendarEvents == null) {
-    calendarEvents = [];
-}
+businessHourStart = parseInt(localStorage.getItem("business-hour-start")) || 9;
+businessHourEnd = parseInt(localStorage.getItem("business-hour-end")) || 18;
+calendarEvents = JSON.parse(localStorage.getItem("calendarEvents")) || [];
 getTodayEvents();
 showTodayPlanner();
